fb7733f91edfd292a3bc3de91f03aa1b416c0e29
YegDevLikeBoss <31534524+YegDevLikeBoss@users.noreply.github.com>
2017-10-30 17:23:08 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,45 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <GL/glew.h>
-#include <Gl/freeglut.h>
+#include <GL/freeglut.h>
 
-void myinit(void) {
-glClearColor(0.5, 0.5, 0.5, 0.0);
-glColor3f(1.0, 0.0, 0.0);
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-gluOrtho2D(0.0, 480.0, 0.0, 640.0);
-glMatrixMode(GL_MODELVIEW);
+#define KEY_ESCAPE 27
+
+int i=0, j=0;
+float fi=0.0f, fj=0.0f;
+
+typedef struct {
+    int width;
+	int height;
+	char* title;
+
+	float field_of_view_angle;
+	float z_near;
+	float z_far;
+} glutWindow;
+
+glutWindow win;
+
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glTranslatef(0.0f,0.0f,-3.0f);
+
+	/*
+	 * Triangle code starts here
+	 * 3 verteces, 3 colors.
+	 */
+    glBegin(GL_TRIANGLES);
+		glColor3f(0.0f,0.0f,1.0f);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f,1.0f,0.0f);
+		glVertex3f(-1.0f,-1.0f, 0.0f);
+		glColor3f(1.0f,0.0f,0.0f);
+		glVertex3f( 1.0f,-1.0f, 0.0f);
+	glEnd();
+	/*glBegin(GL_TRIANGLES);
+		glColor3f(0.0f,0.0f,1.0f);
+		glVertex3f( -2.0f, 0.1f, 0.0f);
+		glColor3f(0.0f,1.0f,0.0f);
+		glVertex3f(-0.1f,-0.1f, 0.0f);
+		glColor3f(0.0f,0.0f,0.0f);
+		glVertex3f( 0.0f,-1.5f, 0.0f);
+	glEnd();
+    for (i=0;i<5;i++)
+    {
+        for (j=0;j<5;j++)
+        {
+            fi*=i;
+            fj*=j;
+            glBegin(GL_TRIANGLES);
+                glColor3f(0.0f,0.0f,1.0f);
+                glVertex3f( fi, 0.1f, 0.0f);
+                glColor3f(0.0f,1.0f,0.0f);
+                glVertex3f(-0.1f,fj, 0.0f);
+                glColor3f(0.0f,0.0f,0.0f);
+                glVertex3f( fj,fi, 0.0f);
+            glEnd();
+        }
+    }*/
+
+	glutSwapBuffers();
 }
 
-void display(void) {
-typedef GLfloat point2[2];
-point2 vertices[3] = {{0.0, 0.0}, {240.0, 640.0}, {480.0, 0.0}};
-int j, k;
-point2 p = {0.0, 0.0};
-glClear(GL_COLOR_BUFFER_BIT);
-for (k = 0; k < 500000; k++) {
-j = rand() % 3;
-p[0] = (p[0] + vertices[j][0]) / 2.0;
-p[1] = (p[1] + vertices[j][1]) / 2.0;
-glBegin(GL_POINTS);
-glVertex2fv(p);
-glEnd();
+
+void initialize ()
+{
+    glMatrixMode(GL_PROJECTION);												// select projection matrix
+    glViewport(0, 0, win.width, win.height);									// set the viewport
+    glMatrixMode(GL_PROJECTION);												// set matrix mode
+    glLoadIdentity();															// reset projection matrix
+    GLfloat aspect = (GLfloat) win.width / win.height;
+	gluPerspective(win.field_of_view_angle, aspect, win.z_near, win.z_far);		// set up a perspective projection matrix
+    glMatrixMode(GL_MODELVIEW);													// specify which matrix is the current matrix
+    glShadeModel( GL_SMOOTH );
+    glClearDepth( 1.0f );														// specify the clear value for the depth buffer
+    glEnable( GL_DEPTH_TEST );
+    glDepthFunc( GL_LEQUAL );
+    glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );						// specify implementation-specific hints
+	glClearColor(0.0, 0.0, 0.0, 1.0);											// specify clear values for the color buffers
 }
-glFlush();
+
+
+void keyboard ( unsigned char key, int mousePositionX, int mousePositionY )
+{
+  switch (key)
+  {
+    case KEY_ESCAPE:
+      exit(0);
+      break;
+
+    default:
+      break;
+  }
 }
 
 int main(int argc, char **argv)
 {
-glutInit(&argc, argv);
-glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-glutInitWindowSize (640, 480);
-glutInitWindowPosition (100, 100);
-glutCreateWindow ("Langton's ant");
-glutDisplayFunc (display);
-myinit();
-glutMainLoop();
+	// set window values
+	win.width = 640;
+	win.height = 480;
+	win.title = "Lengton's ant";
+	win.field_of_view_angle = 45;
+	win.z_near = 1.0f;
+	win.z_far = 500.0f;
 
-return 0;
+	// initialize and run program
+	glutInit(&argc, argv);                                      // GLUT initialization
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );  // Display Mode
+	glutInitWindowSize(win.width,win.height);					// set window size
+	glutCreateWindow(win.title);								// create Window
+	glutDisplayFunc(display);									// register Display Function
+	glutIdleFunc(display);									// register Idle Function
+    glutKeyboardFunc(keyboard);								// register Keyboard Handler
+	initialize();
+	glutMainLoop();												// run GLUT mainloop
+	return 0;
 }
+
