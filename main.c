8940e50c1675f60a0141f198a16ac46b28f0087a
YegDevLikeBoss <31534524+YegDevLikeBoss@users.noreply.github.com>
2017-11-17 17:51:12 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,124 +1,109 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <GL/glew.h>
+#include <time.h>
 #include <GL/freeglut.h>
 
-#define KEY_ESCAPE 27
+int WindW, WindH;
+int i, mem, minus=-1;
+int alpha;
+static int tmx=6, tmy=6;
+int space[600][600];
 
-int i=0, j=0;
-float fi=0.0f, fj=0.0f;
-
-typedef struct {
-    int width;
-	int height;
-	char* title;
-
-	float field_of_view_angle;
-	float z_near;
-	float z_far;
-} glutWindow;
-
-glutWindow win;
-
-
-void display()
+void Reshape(int width, int height) /*Reshape function*/
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	glTranslatef(0.0f,0.0f,-3.0f);
+  glViewport(0, 0, width, height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(-1, 1, -1, 1);
+  glMatrixMode(GL_MODELVIEW);
 
-	/*
-	 * Triangle code starts here
-	 * 3 verteces, 3 colors.
-	 */
-    glBegin(GL_TRIANGLES);
-		glColor3f(0.0f,0.0f,1.0f);
-		glVertex3f( 0.0f, 1.0f, 0.0f);
-		glColor3f(0.0f,1.0f,0.0f);
-		glVertex3f(-1.0f,-1.0f, 0.0f);
-		glColor3f(1.0f,0.0f,0.0f);
-		glVertex3f( 1.0f,-1.0f, 0.0f);
-	glEnd();
-	/*glBegin(GL_TRIANGLES);
-		glColor3f(0.0f,0.0f,1.0f);
-		glVertex3f( -2.0f, 0.1f, 0.0f);
-		glColor3f(0.0f,1.0f,0.0f);
-		glVertex3f(-0.1f,-0.1f, 0.0f);
-		glColor3f(0.0f,0.0f,0.0f);
-		glVertex3f( 0.0f,-1.5f, 0.0f);
-	glEnd();
-    for (i=0;i<5;i++)
+  WindW = width;
+  WindH = height;
+}
+
+void Draw(void) /*Window redraw function*/
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  /*glLineWidth(3);*/
+  glColor3f(0.0f, 0.0f, 0.0f);
+
+  glPushMatrix();
+  /*glRotatef(alpha, 0.0f, 1.0f, 0.0f);
+  alpha += 4;
+  if (alpha > 359) alpha = 0;*/
+    tmx = rand() %100;
+    tmy = rand() %100;
+    minus = rand() %2;
+    if (minus==1)
+        tmx*=-1;
+    minus = rand() %2;
+    if (minus==1)
+        tmy*=-1;
+    glTranslated( ((double)tmx)/10000.0, ((double)tmy)/10000.0, 0);
+
+
+    i++;
+    printf("%d %d)", tmx,tmy);
+
+  glBegin(GL_QUADS);
+    glVertex2f(0.01f, 0.01f);
+    glVertex2f(0.01f, -0.01f);
+    glVertex2f(-0.01f, -0.01f);
+    glVertex2f(-0.01f, 0.01f);
+  glEnd();
+
+  /*glPopMatrix();*/
+
+  /*glFlush();*/
+  glutSwapBuffers();
+}
+
+void Visibility(int state) /*Visibility function*/
+{
+  if (state == GLUT_NOT_VISIBLE) printf("Window not visible!\n");
+  if (state == GLUT_VISIBLE) printf("Window visible!\n");
+}
+
+void timf(int value) /*Timer function*/
+{
+  glutPostRedisplay();  /*Redraw windows*/
+  glutTimerFunc(50, timf, 0); /*Setup next timer*/
+}
+
+/*void matr()
+{
+    int i, j;
+    for (i=0;i<600;i++)
     {
-        for (j=0;j<5;j++)
+        for (i=0;i<600;i++)
         {
-            fi*=i;
-            fj*=j;
-            glBegin(GL_TRIANGLES);
-                glColor3f(0.0f,0.0f,1.0f);
-                glVertex3f( fi, 0.1f, 0.0f);
-                glColor3f(0.0f,1.0f,0.0f);
-                glVertex3f(-0.1f,fj, 0.0f);
-                glColor3f(0.0f,0.0f,0.0f);
-                glVertex3f( fj,fi, 0.0f);
-            glEnd();
+            p
         }
-    }*/
-
-	glutSwapBuffers();
+    }
 }
-
-
-void initialize ()
+*/
+int main(int argc, char *argv[])
 {
-    glMatrixMode(GL_PROJECTION);												// select projection matrix
-    glViewport(0, 0, win.width, win.height);									// set the viewport
-    glMatrixMode(GL_PROJECTION);												// set matrix mode
-    glLoadIdentity();															// reset projection matrix
-    GLfloat aspect = (GLfloat) win.width / win.height;
-	gluPerspective(win.field_of_view_angle, aspect, win.z_near, win.z_far);		// set up a perspective projection matrix
-    glMatrixMode(GL_MODELVIEW);													// specify which matrix is the current matrix
-    glShadeModel( GL_SMOOTH );
-    glClearDepth( 1.0f );														// specify the clear value for the depth buffer
-    glEnable( GL_DEPTH_TEST );
-    glDepthFunc( GL_LEQUAL );
-    glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );						// specify implementation-specific hints
-	glClearColor(0.0, 0.0, 0.0, 1.0);											// specify clear values for the color buffers
+  WindW = 600;
+  WindH = 600;
+  alpha = 1;
+
+    printf("aaaaaaaa");
+
+
+  srand(time(NULL));
+  glutInit(&argc, argv);
+  glutInitWindowSize(WindW, WindH);
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+  glutCreateWindow("Lengton's ant");
+
+  glutReshapeFunc(Reshape); /*Set up reshape function*/
+  glutDisplayFunc(Draw);    /*Set up redisplay function*/
+  glutTimerFunc(100, timf, 0); /*Set up timer for 40ms, about 25 fps*/
+  glutVisibilityFunc(Visibility); /*Set up visibility funtion*/
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+  glutMainLoop();
+
+  return 0;
 }
-
-
-void keyboard ( unsigned char key, int mousePositionX, int mousePositionY )
-{
-  switch (key)
-  {
-    case KEY_ESCAPE:
-      exit(0);
-      break;
-
-    default:
-      break;
-  }
-}
-
-int main(int argc, char **argv)
-{
-	// set window values
-	win.width = 640;
-	win.height = 480;
-	win.title = "Lengton's ant";
-	win.field_of_view_angle = 45;
-	win.z_near = 1.0f;
-	win.z_far = 500.0f;
-
-	// initialize and run program
-	glutInit(&argc, argv);                                      // GLUT initialization
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );  // Display Mode
-	glutInitWindowSize(win.width,win.height);					// set window size
-	glutCreateWindow(win.title);								// create Window
-	glutDisplayFunc(display);									// register Display Function
-	glutIdleFunc(display);									// register Idle Function
-    glutKeyboardFunc(keyboard);								// register Keyboard Handler
-	initialize();
-	glutMainLoop();												// run GLUT mainloop
-	return 0;
-}
-
