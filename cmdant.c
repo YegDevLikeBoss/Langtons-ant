4a8bcf820816bfb1d493dc84015cc1e5cdb62626
YegDevLikeBoss <31534524+YegDevLikeBoss@users.noreply.github.com>
2017-11-19 15:39:37 +0300

    Add files via upload

--- cmdant.c
+++ cmdant.c
@@ -2,54 +2,100 @@
 #include <stdlib.h>
 
 #define SPACE 30
+#define INIT 10
 
 int space[SPACE][SPACE];
+int x=SPACE/2, y=SPACE/2, ant_up, init=0;
 
 void matformat(char at);
+int up()
+{
+    if (init>INIT)
+    {
+        return 1;
+    }
+    y--;
+    if (space[x][y]==0)
+        space[x][y]=1;
+        else space[x][y]=0;
+    matformat('d');
+
+
+    if (space[x][y]!=0) {
+
+        right(); }
+        else
+            left();
+    init++;
+
+
+}
+void down()
+{
+    y++;
+    if (space[x][y]==0)
+        space[x][y]=1;
+        else space[x][y]=0;
+    matformat('d');
+
+
+    if (space[x][y]!=0) {
+
+        left(); }
+        else
+            right();
+    init++;
+
+
+}
+void left()
+{
+    x--;
+    if (space[x][y]==0)
+        space[x][y]=1;
+        else space[x][y]=0;
+    matformat('d');
+
+
+    if (space[x][y]!=0) {
+
+        up(); }
+        else
+            down();
+
+    init++;
+
+
+}
+void right()
+{
+    x++;
+    if (space[x][y]==0)
+        space[x][y]=1;
+        else space[x][y]=0;
+    matformat('d');
+
+
+    if (space[x][y]!=0) {
+
+        down(); }
+        else
+            up();
+
+    init++;
+
+
+}
+
 
 int main()
 {
-    int x=SPACE/2, y=SPACE/2, count=0, init=0, neg=-1, x_num=1, y_num=1;
     matformat('0');
     matformat('d');
+    up();
 
-    while (init<300)
-    {
-        if (x<0)
-            x=SPACE-1;
-            else if (x>=SPACE)
-                x=0;
-        if (y<0)
-            y=SPACE-1;
-            else if (y>=SPACE)
-                y=0;
-        if (space[y][x]==0)
-            space[y][x]=1;
-            else
-                space[y][x]=0;
-        init++;
-        matformat('d');
-        if (count==0)
-        {
-            count=1;
-            y+=y_num;
-            if (space[y][x]==0)
-                x_num*=neg;
-            /*if (y_num==-1)
-                x_num*=neg;*/
-        } else
-
-        if (count==1)
-        {
-            count=0;
-            x+=x_num;
-            if (space[y][x]==0)
-                y_num*=neg;
-            /*if (x_num==-1)
-                y_num*=neg;*/
-        }
-
-    }
+    if (init>INIT)
+        return 1;
     matformat('d');
     return 1;
 }
@@ -73,10 +119,11 @@ void matformat(char at)
         {
             for (j=0;j<SPACE;j++)
             {
-                printf("%d ", space[i][j]);
+                printf("%d ", space[j][i]);
             }
             printf("\n");
         }
         printf("\n");
     }
 }
+
